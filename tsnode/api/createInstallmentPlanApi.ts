132773de4f48c84afde1f17e7f1e29f22df01cad
Jenkins <jenkins@splitit.com>
2022-02-15 07:04:03 +0000

    New Commit from Jenkins Build #17

--- tsnode/api/createInstallmentPlanApi.ts
+++ tsnode/api/createInstallmentPlanApi.ts
@@ -37,7 +37,7 @@ export class CreateInstallmentPlanApi {
     constructor(config: Configuration, sessionId?: string) {
         this._config = config;
         this._defaultHeaders = {
-            "Splitit-SDK": "NodeJS-1.6.5516"
+            "Splitit-SDK": "NodeJS-1.6.5517"
         };
         this._sessionId = sessionId;
     }
